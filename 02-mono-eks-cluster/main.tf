83c11a5611e94c764af90549afc10b64509b9398
eduard <ejsotelo4@gmail.com>
2024-10-21 12:38:05 -0500

    ajuste en el archivo main.tf para el despliegue de EKS

--- 02-mono-eks-cluster/main.tf
+++ 02-mono-eks-cluster/main.tf
@@ -31,24 +31,31 @@ module "vpc" {
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = "mono-eks-cluster"
   cluster_version = "1.30"
-
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
   eks_managed_node_groups = {
     nodes = {
       min_size     = 1
       max_size     = 3
-      desired_size = 2
-
+      desired_size = 1
       instance_type = ["t2.micro"]
     }
   }
+
+  enable_cluster_creator_admin_permissions = true
 
   tags = {
     Environment = "dev"
